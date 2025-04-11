876044b6e81b289c1a02e4793a41cfe832143712
Rishabh Goyal <goyalrishabh1309@gmail.com>
2025-04-11 00:38:26 -0700

    Register class created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Components/Login";
+import Register from "./Components/Register";
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,26 +1,64 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
-const registerState = {username: '', password: ''}
+const registerState = { username: "", password: "" };
 function Register() {
-    const [formData, setFormData] = useState(registerState);
-    const [message, setMessage] = useState('');
-    const navigate = useNavigate();
-    
-    const Register = (e) => {
-        e.preventDefault();
+  const [formData, setFormData] = useState(registerState);
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-        try {
+  const handleRegister = async (e) => {
+    e.preventDefault();
 
-        }
-    };
-    
-    export default Register
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/api/auth/register",
+        formData
+      );
+      setMessage(res.data);
 
+      if (res.data === "Register successfully!") {
+        navigate("/login");
+      }
+    } catch (err) {
+      setMessage("Registration failed");
+    }
+  };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-
+  return (
+    <div className = "container">
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="Submit">Register</button>
+      </form>
+      <p>{message}</p>
+      <p>
+        {" "}
+        Already have an account ? <Link to="/login"> Login Here</Link>
+      </p>
+    </div>
+  );
 }
 
-
+export default Register;
